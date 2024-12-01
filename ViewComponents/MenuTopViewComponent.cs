0d1e7928189cd4184e79fdaf57327e34c7f6bc6b
lengoctam2004m <163614089+lengoctam2004m@users.noreply.github.com>
2024-12-01 23:47:52 +0700

    Add files via upload

---
+++ ViewComponents/MenuTopViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Nhom15.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Harmic.ViewComponents
+{
+	public class MenuTopViewComponent : ViewComponent
+	{
+		private readonly Nhom15Context _context;
+		public MenuTopViewComponent(Nhom15Context context)
+		{
+			_context = context;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var items = _context.TbMenus.Where(m => (bool)m.IsActive).
+				OrderBy(m => m.Position).ToList();
+			return await Task.FromResult<IViewComponentResult>(View(items));
+		}
+	}
+}
